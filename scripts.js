b4088c07e06aa73ae228c4480cef75246d9edfae
RupinVijan <rupinvijan@gmail.com>
2021-08-12 23:19:26 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,40 @@
+// const submitEl=document.getElementById('submit');
+let total=0;
+// submitEl.addEventListener("click",()=>{
+// });
+function displayRadioValue(){
+    document.getElementById("hh").innerHTML='<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>';
+    
+    total+=500;
+    document.getElementById("total").innerHTML= "TOTAL = " + total;
+    console.log("hihi")
+
+
+}
+function displayRadioValue1(){
+    document.getElementById("hh1").innerHTML='<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>';
+    
+    total+=500;
+    document.getElementById("total").innerHTML= "TOTAL = " + total;
+    console.log("hihi")
+
+
+}
+function displayRadioValue2(){
+    document.getElementById("hh2").innerHTML='<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>';
+    
+    total+=500;
+    document.getElementById("total").innerHTML= "TOTAL = " + total;
+    console.log("hihi")
+
+
+}
+function displayRadioValue3(){
+    document.getElementById("hh3").innerHTML='<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>';
+    
+    total+=500;
+    document.getElementById("total").innerHTML= "TOTAL = " + total;
+    console.log("hihi")
+
+
+}
